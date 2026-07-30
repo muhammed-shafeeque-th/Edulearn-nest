6f2f7a1f578d0161a4f3de7e9188fc297e6cbeaa
shafeeque <shafeeqsha06@gmail.com>
2026-07-30 21:52:47 +0530

    feat(logger): update logger service, removed redudant method defenitions

--- src/observability/logger/logger.service.ts
+++ src/observability/logger/logger.service.ts
@@ -11,26 +11,4 @@ export class LoggerService extends Logger_Service {
     super(logger);
   }
 
-  info(message: string, meta?: unknown) {
-    this.logger.info(message, meta);
-  }
-
-  warn(message: string, meta?: unknown) {
-    this.logger.warn(message, meta);
-  }
-
-  error(message: string, trace?: unknown, meta?: Record<string, unknown>) {
-    this.logger.error(message, {
-      trace,
-      ...meta,
-    });
-  }
-
-  debug(message: string, meta?: unknown) {
-    this.logger.debug(message, meta);
-  }
-
-  child(meta: Record<string, unknown>) {
-    return this.logger.child(meta);
-  }
 }
